a3a3569bdc967083cb9771cf68d7b71a6e166d62
danielafontal03 <daniela.fontal@correounivalle.edu.co>
2025-09-29 01:53:18 -0500

    Update UserDAO.js

--- api/dao/UserDAO.js
+++ api/dao/UserDAO.js
@@ -1,14 +1,15 @@
 const User = require("../models/User");
 const GlobalDAO = require("./GlobalDAO");
 
-
+// UserDAO extends the generic GlobalDAO to handle User-specific persistence
 class UserDAO extends GlobalDAO{
   constructor() {
+    // Pass the User model to the parent DAO
     super(User);
   }
 }
 
-
+// Export a singleton instance of UserDAO
 module.exports = new UserDAO();
 
 
