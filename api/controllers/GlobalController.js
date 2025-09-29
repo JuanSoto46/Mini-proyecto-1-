f703d44b4c38098f03252b3b5bb474f1fef946ea
danielafontal03 <daniela.fontal@correounivalle.edu.co>
2025-09-29 01:25:04 -0500

    Update GlobalController.js

--- api/controllers/GlobalController.js
+++ api/controllers/GlobalController.js
@@ -1,8 +1,9 @@
 class GlobalController {
   constructor(dao) {
-    this.dao = dao;
+    this.dao = dao; // Data Access Object to interact with persistence layer
   }
 
+  // Create a new item
   async create(req, res) {
     console.log("Creating item with data:", req.body);
     try {
@@ -13,6 +14,7 @@ class GlobalController {
     }
   }
 
+  // Read an item by ID
   async read(req, res) {
     try {
       const item = await this.dao.read(req.params.id);
@@ -22,6 +24,7 @@ class GlobalController {
     }
   }
 
+  // Update an existing item by ID
   async update(req, res) {
     try {
       const item = await this.dao.update(req.params.id, req.body);
@@ -31,6 +34,7 @@ class GlobalController {
     }
   }
 
+  // Delete an item by ID
   async delete(req, res) {
     try {
       const item = await this.dao.delete(req.params.id);
@@ -40,6 +44,7 @@ class GlobalController {
     }
   }
 
+  // Retrieve all items, with optional filters from query params
   async getAll(req, res) {
     try {
       const items = await this.dao.getAll(req.query);
